3db3a106ff177632d6859694bd1af557e188005c
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-11-22 13:04:40 +0100

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,69 +1,80 @@
-# import necessary libraries
+# Hey, aquí está mi intento de análisis estadístico en Python. Espero que funcione.
+
+# Importar las bibliotecas necesarias
 import numpy as np
-from scipy import stats  # ¡Agrega esta línea!
+from scipy import stats  
 
-# function to calculate mean
-def calculate_mean(data):
-    return np.mean(data)
+# Función para calcular la media
+def calcular_media(datos):
+    return np.mean(datos)
 
-# function to calculate standard deviation
-def calculate_standard_deviation(data):
-    return np.std(data)
+# Función para calcular la desviación estándar
+def calcular_desviacion_estandar(datos):
+    return np.std(datos)
 
-# function to perform one-sample t-test
-def one_sample_t_test(data, null_hypothesis_mean):
-    t_stat, p_value = stats.ttest_1samp(data, null_hypothesis_mean)
-    return t_stat, p_value
+# Función para hacer una prueba t de una muestra
+def prueba_t_una_muestra(datos, media_nula):
+    t_estadistico, valor_p = stats.ttest_1samp(datos, media_nula)
+    return t_estadistico, valor_p
 
-# function to perform two-sample t-test
-def two_sample_t_test(data1, data2):
-    t_stat, p_value = stats.ttest_ind(data1, data2)
-    return t_stat, p_value
+# Función para hacer una prueba t de dos muestras
+def prueba_t_dos_muestras(datos1, datos2):
+    t_estadistico, valor_p = stats.ttest_ind(datos1, datos2)
+    return t_estadistico, valor_p
 
-# function to perform chi-squared test
-def chi_squared_test(observed, expected):
-    chi_stat, p_value = stats.chisquare(f_obs=observed, f_exp=expected)
-    return chi_stat, p_value
+# Función para hacer una prueba chi-cuadrado
+def prueba_chi_cuadrado(observado, esperado):
+    chi_estadistico, p_value = stats.chisquare(f_obs=observado, f_exp=esperado)
+    return chi_estadistico, p_value
 
-# main function to run the program
-def main():
-    print("Welcome to the Statistical Analysis Program!")
-    
-    # input experimental data
-    data = list(map(float, input("Enter your experimental data separated by spaces: ").split()))
-    
-    # calculate and display mean
-    mean = calculate_mean(data)
-    print(f"Mean: {mean}")
-    
-    # calculate and display standard deviation
-    standard_deviation = calculate_standard_deviation(data)
-    print(f"Standard Deviation: {standard_deviation}")
-    
-    # hypothesis testing options
-    print("\nHypothesis Testing:")
-    print("1. One-sample t-test")
-    print("2. Two-sample t-test")
-    print("3. Chi-squared test")
-    
-    test_choice = int(input("Choose a hypothesis test (1/2/3): "))
-    
-    if test_choice == 1:
-        null_mean = float(input("Enter the null hypothesis mean: "))
-        t_stat, p_value = one_sample_t_test(data, null_mean)
-        print(f"One-sample t-test: t_stat = {t_stat}, p_value = {p_value}")
-    elif test_choice == 2:
-        data2 = list(map(float, input("Enter the second set of data for two-sample t-test: ").split()))
-        t_stat, p_value = two_sample_t_test(data, data2)
-        print(f"Two-sample t-test: t_stat = {t_stat}, p_value = {p_value}")
-    elif test_choice == 3:
-        observed = list(map(float, input("Enter the observed frequencies separated by spaces: ").split()))
-        expected = list(map(float, input("Enter the expected frequencies separated by spaces: ").split()))
-        chi_stat, p_value = chi_squared_test(observed, expected)
-        print(f"Chi-squared test: chi_stat = {chi_stat}, p_value = {p_value}")
+# Función principal para correr el programa
+def principal():
+    print("¡Bienvenido al Programa de Análisis Estadístico!")
+
+    # Ingresar datos experimentales
+    datos = list(map(float, input("Ingresa tus datos experimentales separados por espacios: ").split()))
+
+    # Calcular y mostrar la media
+    media = calcular_media(datos)
+    print(f"Media: {media}")
+
+    # Calcular y mostrar la desviación estándar
+    desviacion_estandar = calcular_desviacion_estandar(datos)
+    print(f"Desviación Estándar: {desviacion_estandar}")
+
+    # Opciones de pruebas de hipótesis
+    print("\nPruebas de Hipótesis:")
+    print("1. Prueba t de una muestra")
+    print("2. Prueba t de dos muestras")
+    print("3. Prueba chi-cuadrado")
+    # Preguntar al usuario que elija una prueba de hipótesis (1/2/3)
+    eleccion_prueba = int(input("Elige una prueba de hipótesis (1/2/3): "))
+
+    if eleccion_prueba == 1:
+        # Para la prueba t de una muestra
+        media_nula = float(input("Ingresa la media nula para la prueba t de una muestra: "))
+        t_estadistico, valor_p = prueba_t_una_muestra(datos, media_nula)
+        print(f"Prueba t de una muestra: t_estadistico = {t_estadistico}, valor_p = {valor_p}")
+
+    elif eleccion_prueba == 2:
+        #Para la prueba t de dos muestras
+        datos2 = list(map(float, input("Ingresa el segundo conjunto de datos para la prueba t de dos muestras: ").split()))
+        t_estadistico, valor_p = prueba_t_dos_muestras(datos, datos2)
+        print(f"Prueba t de dos muestras: t_estadistico = {t_estadistico}, valor_p = {valor_p}")
+
+    elif eleccion_prueba == 3:
+        #Para la prueba de chi-cuadrado
+        observado = list(map(float, input("Ingresa las frecuencias observadas separadas por espacios: ").split()))
+        esperado = list(map(float, input("Ingresa las frecuencias esperadas separadas por espacios: ").split()))
+        chi_estadistico, valor_p = prueba_chi_cuadrado(observado, esperado)
+        print(f"Prueba chi-cuadrado: chi_estadistico = {chi_estadistico}, valor_p = {valor_p}")
+        
     else:
-        print("Invalid choice. Exiting program.")
+        print("Opción no válida. Saliendo del programa.")
 
-# run the program
+# Ejecutar el programa
 if __name__ == "__main__":
-    main()
+    principal()
+
+
+
